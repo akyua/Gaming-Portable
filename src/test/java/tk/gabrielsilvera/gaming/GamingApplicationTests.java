0526e44d14d45f5be557888028d736869c9b088a
akyua <gabrielnovatoo@gmail.com>
2023-09-19 09:57:38 -0300

    stater spring project

---
+++ src/test/java/tk/gabrielsilvera/gaming/GamingApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.gabrielsilvera.gaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
